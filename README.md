cb913a84c212ec561887e525f6e130f534dad645
willwoodson <201782016@mail.dlut.edu.cn>
2020-04-19 19:24:49 +0800

    原始代码

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# som-tsp
+# 使用自组织映射解决旅行商问题
+
+## 作者: Diego Vicente Martín
+### EMAIL:  mail@diego.codes
+
+该存储库包含自组织映射的实现，该映射可用于查找旅行商问题的次优解决方案。
+程序支持的问题的实例是 **.tsp** 文件，这是此问题中的一种普遍格式。
+所有源代码都可以在 **src** 目录中找到，而报告和简短演示幻灯片（西班牙语）可以在 **report** 文件夹中找到。
+但是，要全面阅读该主题， 你可以阅读我的[博客文章](https://diego.codes/post/som-tsp/)。
+
+![算法演示](diagrams/uruguay.gif)
+
+
+要运行代码，只需要Python 3和依赖项（默认情况下包含在Anaconda发行版中的 **matplotlib**，**numpy** 和 **pandas**）。如果您不使用Anaconda，则可以使用以下命令安装所有依赖项：
+
+```bash
+pip install -r requirements.txt
+```
+
+要运行代码，只需执行以下命令：
+
+```bash
+cd som-tsp 
+python src/main.py assets/<instance>.tsp
+```
+
+生成的图像将存储在**diagrams**文件夹中。
+使用**convert**之类的工具，您可以通过运行以下命令轻松生成类似于此文件中的动画的动画：
+
+```bash
+cd som-tsp 
+convert -delay 10 -loop 0 *.png animation.gif
+```
+
+该代码已获得MIT许可，因此可以在您的项目中随意修改和/或使用它。
+如果您有任何疑问，请随时与我联系或通过创建问题来为此存储库做出贡献。
+
+-----
+
+该代码是针对计算机科学与技术硕士学位@ UC3M的生物启发式人工智能课程提供的。
+可以在[此存储库](https://github.com/DiegoVicen/ntnu-som)中找到此代码的先前版本。
+特别感谢[Leonard Kleinans](https://github.com/leo-labs)，他在之前的版本中与我合作。
---
+++ src/main.py
@@ -0,0 +1,78 @@
+from sys import argv
+
+import numpy as np
+
+from io_helper import read_tsp, normalize
+from neuron import generate_network, get_neighborhood, get_route
+from distance import select_closest, euclidean_distance, route_distance
+from plot import plot_network, plot_route
+
+def main():
+    if len(argv) != 2:
+        print("Correct use: python src/main.py <filename>.tsp")
+        return -1
+
+    problem = read_tsp(argv[1])
+
+    route = som(problem, 100000)
+
+    problem = problem.reindex(route)
+
+    distance = route_distance(problem)
+
+    print('Route found of length {}'.format(distance))
+
+
+def som(problem, iterations, learning_rate=0.3):
+    """Solve the TSP using a Self-Organizing Map."""
+
+    # Obtain the normalized set of cities (w/ coord in [0,1])
+    cities = problem.copy()
+
+    cities[['x', 'y']] = normalize(cities[['x', 'y']])
+
+    # The population size is 8 times the number of cities
+    n = cities.shape[0] * 8
+
+    # Generate an adequate network of neurons:
+    network = generate_network(n)
+    print('Network of {} neurons created. Starting the iterations:'.format(n))
+
+    for i in range(iterations):
+        if not i % 100:
+            print('\t> Iteration {}/{}'.format(i, iterations), end="\r")
+        # Choose a random city
+        city = cities.sample(1)[['x', 'y']].values
+        winner_idx = select_closest(network, city)
+        # Generate a filter that applies changes to the winner's gaussian
+        gaussian = get_neighborhood(winner_idx, n//10, network.shape[0])
+        # Update the network's weights (closer to the city)
+        network += gaussian[:,np.newaxis] * learning_rate * (city - network)
+        # Decay the variables
+        learning_rate = learning_rate * 0.99997
+        n = n * 0.9997
+
+        # Check for plotting interval
+        if not i % 1000:
+            plot_network(cities, network, name='diagrams/{:05d}.png'.format(i))
+
+        # Check if any parameter has completely decayed.
+        if n < 1:
+            print('Radius has completely decayed, finishing execution',
+            'at {} iterations'.format(i))
+            break
+        if learning_rate < 0.001:
+            print('Learning rate has completely decayed, finishing execution',
+            'at {} iterations'.format(i))
+            break
+    else:
+        print('Completed {} iterations.'.format(iterations))
+
+    plot_network(cities, network, name='diagrams/final.png')
+
+    route = get_route(cities, network)
+    plot_route(cities, route, 'diagrams/route.png')
+    return route
+
+if __name__ == '__main__':
+    main()
