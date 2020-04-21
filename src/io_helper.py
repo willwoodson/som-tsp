e70d754404e119fc9917d706627d54d9b730e23e
willwoodson <201782016@mail.dlut.edu.cn>
2020-04-21 21:58:08 +0800

    修改io_helper

--- src/io_helper.py
+++ src/io_helper.py
@@ -1,56 +1,52 @@
 import pandas as pd
 import numpy as np
 
+
 def read_tsp(filename):
     """
-    Read a file in .tsp format into a pandas DataFrame
-
-    The .tsp files can be found in the TSPLIB project. Currently, the library
-    only considers the possibility of a 2D map.
+    读取.tsp文件中的二维城市坐标到pandas DataFrame中
     """
     with open(filename) as f:
-        node_coord_start = None
-        dimension = None
+        # 读取整个文件所有行，保存在一个列表(list)变量中
         lines = f.readlines()
+        dimension, node_coord_start, i = 0, 0, 0
 
-        # Obtain the information about the .tsp
-        i = 0
-        while not dimension or not node_coord_start:
+        # 读取tsp问题城市维度，二维城市坐标起始位
+        while not node_coord_start:
             line = lines[i]
-            if line.startswith('DIMENSION :'):
-                dimension = int(line.split()[-1])
-            if line.startswith('NODE_COORD_SECTION'):
-                node_coord_start = i
-            i = i+1
+            if line.startswith("DIMENSION :"):
+                dimension = int(line.split(" ")[-1])
+            if line.startswith("NODE_COORD_SECTION"):
+                node_coord_start = i + 1
+            i = i + 1
 
-        print('Problem with {} cities read.'.format(dimension))
+        print("Problem with {} cities read.".format(dimension))
 
-        f.seek(0)
-
-        # Read a data frame out of the file descriptor
+        # 读取二维城市坐标
+        f.seek(0)  # 从文件头开始读取
         cities = pd.read_csv(
             f,
-            skiprows=node_coord_start + 1,
-            sep=' ',
-            names=['city', 'y', 'x'],
-            dtype={'city': str, 'x': np.float64, 'y': np.float64},
-            header=None,
-            nrows=dimension
+            skiprows=node_coord_start,  # 需要忽略的行数（从文件开始处算起）
+            sep=" ",  # 指定分隔符
+            names=["city", "x", "y"],  # 列名,数据类型
+            dtype={"city": str, "x": np.float64, "y": np.float64},
+            header=None,  # 指定行数用来作为列名，已经设定列名，置None
+            nrows=dimension,  # 需要读取的行数
         )
-
-        # cities.set_index('city', inplace=True)
+        cities.to_csv("data/cities.csv")
 
         return cities
 
+
 def normalize(points):
     """
-    Return the normalized version of a given vector of points.
+    返回给定点向量的规范化版本
 
-    For a given array of n-dimensions, normalize each dimension by removing the
-    initial offset and normalizing the points in a proportional interval: [0,1]
-    on y, maintining the original ratio on x.
+    对于给定的n维数组，通过删除初始偏移并按比例间隔对点进行归一化来标准化每个维度：
+    x，y坐标归一到[0,1]后，乘以比例系数以保持x，y坐标的原始比例。
     """
-    ratio = (points.x.max() - points.x.min()) / (points.y.max() - points.y.min()), 1
+    ratio = 1, (points.y.max() - points.y.min()) / (points.x.max() - points.x.min())
     ratio = np.array(ratio) / max(ratio)
-    norm = points.apply(lambda c: (c - c.min()) / (c.max() - c.min()))
-    return norm.apply(lambda p: ratio * p, axis=1)
+    norm = points.apply(lambda p: (p - p.min()) / (p.max() - p.min()))  # 默认以列为单位生成
+    norm = norm.apply(lambda p: ratio * p, axis=1)  # 以行为单位生成
+    return norm
--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
 from sys import argv
-
 import numpy as np
 
 from io_helper import read_tsp, normalize
@@ -7,72 +6,78 @@ from neuron import generate_network, get_neighborhood, get_route
 from distance import select_closest, euclidean_distance, route_distance
 from plot import plot_network, plot_route
 
+
 def main():
     if len(argv) != 2:
         print("Correct use: python src/main.py <filename>.tsp")
         return -1
 
-    problem = read_tsp(argv[1])
+    cities = read_tsp(argv[1])
 
-    route = som(problem, 100000)
+    route = som(cities, 100000)
 
-    problem = problem.reindex(route)
+    cities = cities.reindex(route)
 
-    distance = route_distance(problem)
+    distance = route_distance(cities)
 
-    print('Route found of length {}'.format(distance))
+    print("Route found of length {}".format(distance))
 
 
-def som(problem, iterations, learning_rate=0.3):
-    """Solve the TSP using a Self-Organizing Map."""
+def som(cities, iterations, learning_rate=0.3):
+    """
+    使用自组织映射解决旅行商问题
+    """
+    # 获得标准化的城市集 (w/ coord in [0,1])
+    cities = cities.copy()  # 复制一份用于标准化，不修改原始数据
+    cities[["x", "y"]] = normalize(cities[["x", "y"]])
 
-    # Obtain the normalized set of cities (w/ coord in [0,1])
-    cities = problem.copy()
-
-    cities[['x', 'y']] = normalize(cities[['x', 'y']])
-
-    # The population size is 8 times the number of cities
+    # 神经元数设置为城市数的8倍
     n = cities.shape[0] * 8
 
     # Generate an adequate network of neurons:
     network = generate_network(n)
-    print('Network of {} neurons created. Starting the iterations:'.format(n))
+    print("Network of {} neurons created. Starting the iterations:".format(n))
 
     for i in range(iterations):
         if not i % 100:
-            print('\t> Iteration {}/{}'.format(i, iterations), end="\r")
+            print("\t> Iteration {}/{}".format(i, iterations), end="\r")
         # Choose a random city
-        city = cities.sample(1)[['x', 'y']].values
+        city = cities.sample(1)[["x", "y"]].values
         winner_idx = select_closest(network, city)
         # Generate a filter that applies changes to the winner's gaussian
-        gaussian = get_neighborhood(winner_idx, n//10, network.shape[0])
+        gaussian = get_neighborhood(winner_idx, n // 10, network.shape[0])
         # Update the network's weights (closer to the city)
-        network += gaussian[:,np.newaxis] * learning_rate * (city - network)
+        network += gaussian[:, np.newaxis] * learning_rate * (city - network)
         # Decay the variables
         learning_rate = learning_rate * 0.99997
         n = n * 0.9997
 
         # Check for plotting interval
         if not i % 1000:
-            plot_network(cities, network, name='diagrams/{:05d}.png'.format(i))
+            plot_network(cities, network, name="diagrams/{:05d}.png".format(i))
 
         # Check if any parameter has completely decayed.
         if n < 1:
-            print('Radius has completely decayed, finishing execution',
-            'at {} iterations'.format(i))
+            print(
+                "Radius has completely decayed, finishing execution",
+                "at {} iterations".format(i),
+            )
             break
         if learning_rate < 0.001:
-            print('Learning rate has completely decayed, finishing execution',
-            'at {} iterations'.format(i))
+            print(
+                "Learning rate has completely decayed, finishing execution",
+                "at {} iterations".format(i),
+            )
             break
     else:
-        print('Completed {} iterations.'.format(iterations))
+        print("Completed {} iterations.".format(iterations))
 
-    plot_network(cities, network, name='diagrams/final.png')
+    plot_network(cities, network, name="diagrams/final.png")
 
     route = get_route(cities, network)
-    plot_route(cities, route, 'diagrams/route.png')
+    plot_route(cities, route, "diagrams/route.png")
     return route
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
